fce07cdb63476f9c4899ef588f93a8524afc9e68
Koło <poprostukolo@gmail.com>
2024-02-06 23:05:42 +0100

    Added AOS and back btn

--- js/main.js
+++ js/main.js
@@ -1,5 +1,24 @@
 const foooterSpan = document.querySelector('.footer__location--date')
 const body = document.querySelector('body')
+const backBtn = document.querySelector('.backBtn')
 
+// YEAR IN FOOTER
 let year = new Date().getFullYear()
 foooterSpan.textContent = year
+
+// SHOW BACK BTN
+const showBackBtn = () => {
+	if (window.scrollY >= 900) {
+		backBtn.classList.add('show')
+	} else {
+		backBtn.classList.remove('show')
+	}
+}
+
+//INITIALIZE AOS
+AOS.init({
+	duration: 800,
+	once: true,
+})
+
+document.addEventListener('scroll', showBackBtn)
