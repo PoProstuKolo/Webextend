89ba9649803009a63b7b7cf2fa360245513f3893
Koło <poprostukolo@gmail.com>
2024-02-10 01:06:36 +0100

    Added some functions on main site, and added projects site

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const foooterSpan = document.querySelector('.footer__location--date')
 const body = document.querySelector('body')
 const backBtn = document.querySelector('.backBtn')
-
+const nav = document.querySelector('.navbar')
 // YEAR IN FOOTER
 let year = new Date().getFullYear()
 foooterSpan.textContent = year
@@ -9,9 +9,9 @@ foooterSpan.textContent = year
 // SHOW BACK BTN
 const showBackBtn = () => {
 	if (window.scrollY >= 900) {
-		backBtn.classList.add('show')
+		backBtn.classList.add('showBackBtn')
 	} else {
-		backBtn.classList.remove('show')
+		backBtn.classList.remove('showBackBtn')
 	}
 }
 
@@ -21,4 +21,27 @@ AOS.init({
 	once: true,
 })
 
+//ADD SHADOW TO NAV
+const addBg = () => {
+	if (window.scrollY >= 165) {
+		nav.classList.add('darkBg')
+	} else {
+		nav.classList.remove('darkBg')
+	}
+}
+
+const widthBg = () => {
+	if (window.innerWidth <= 991) {
+		nav.classList.add('darkBg')
+	} else {
+		nav.classList.remove('darkBg')
+	}
+}
+
 document.addEventListener('scroll', showBackBtn)
+document.addEventListener('scroll', () => {
+	if (window.innerWidth >= 992) {
+		addBg()
+	}
+})
+document.addEventListener('DOMContentLoaded', widthBg)
